40352985e681d9aafb867a1d24b957637d0a3e41
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-17 13:20:33 -0700

    validateRoleName

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -19,7 +19,7 @@ const restricted = (req, res, next) => {
   // } else {
   //   next({status: 401, message: "Token required"})
   // }
-next()
+  next()
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -54,13 +54,13 @@ const only = role_name => (req, res, next) => {
 
 const checkUsernameExists = (req, res, next) => {
   //here
-  const {username} = req.body
-  if(username) {
-    next()
-  } else {
-    next({status: 401, message: "Invalid credentials"})
-  }
-
+  // const {username} = req.body
+  // if(username) {
+  //   next()
+  // } else {
+  //   next({status: 401, message: "Invalid credentials"})
+  // }
+  next()
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -72,6 +72,17 @@ const checkUsernameExists = (req, res, next) => {
 
 
 const validateRoleName = (req, res, next) => {
+if (!req.body.role_name || !req.body.role_name.trim()) {
+  role_name = "student"
+  next()
+} else if (req.body.role_name.trim() === 'admin') {
+  next({status: 422, message: "Role name can not be admin"})
+}  else if (req.body.role_name.trim().length > 32 ) {
+  next({status: 422, message: "Role name can not be longer than 32 chars"})
+} else {
+  next()
+}
+
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
@@ -90,6 +101,7 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
+ 
 }
 
 module.exports = {
