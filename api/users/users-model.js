aeec55202147fae4ac2f7e96cd836e537cafeaaf
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-17 12:05:16 -0700

    findById on user-model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -57,6 +57,12 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
+  return db('users as u')
+  .join('roles as r','u.role_id', '=', 'r.role_id')
+  .select('u.user_id', 'u.username', 'r.role_name as role_name')
+  .where('u.user_id', user_id)
+  .first()
+
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
